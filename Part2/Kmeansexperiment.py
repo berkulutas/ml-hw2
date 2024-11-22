4b174e42201527c54f8bf9f5d51e82c05bed7ab8
berkulutas <berkulutas2001@gmail.com>
2024-11-23 02:43:29 +0300

    k means loss implemented

--- Part2/Kmeansexperiment.py
+++ Part2/Kmeansexperiment.py
@@ -7,13 +7,61 @@ from sklearn.cluster import KMeans
 dataset1 = pickle.load(open("../datasets/part2_dataset_1.data", "rb"))
 dataset2 = pickle.load(open("../datasets/part2_dataset_2.data", "rb"))
 
+TIMES = 3 # TODO make it 10
+
+def calc_confidence_interval(data):
+    mean = np.mean(data)
+    std = np.std(data)
+    n = len(data)
+    h = 1.96 * std / np.sqrt(n)
+    return (mean - h), (mean + h)
+
+def get_min_kmeans_loss(dataset, k):
+    min_loss = np.inf
+
+    for _ in range(TIMES):
+        km = KMeans(n_clusters=k)
+        km.fit(dataset)
+        min_loss = min(min_loss, km.inertia_)
+    
+    return min_loss
+
+def calc_avg_kmeans_loss(dataset, k):
+    losses = []
+
+    for _ in range(TIMES):
+        losses.append(get_min_kmeans_loss(dataset, k))
+
+    return np.mean(losses), calc_confidence_interval(losses)
+
+
+def save_plot(title, xlabel, ylabel , xs, ys):
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.plot(xs, ys)
+    plt.grid()
+    plt.savefig(f'plots/{title}') # TODO remove plots save location for grading
+
+
+def calc_kmeans(dataset, k_range):
+    avg_losses = []
+    loss_conf_intervals = []
+
+    for k in k_range:
+        print(f"Running K means for k = {k}")
+        avg_loss, conf_int = calc_avg_kmeans_loss(dataset, k)
+        avg_losses.append(avg_loss)
+        loss_conf_intervals.append(conf_int)
+
+        # report results
+        # TODO better reporting to a csv file
+        print(f"Average Loss = {round(avg_loss,3)}, Confidence Interval = {round(float(conf_int[0]),3), round(float(conf_int[1]),3)}")
+
+    return avg_losses
+
+# plot
 k_range = range(2,11)
-
-losses = []
-for k in k_range:
-    km = KMeans(n_clusters=k)
-    km.fit(dataset1)
-    losses.append(km.inertia_)
-
-plt.plot(k_range, losses)
-plt.savefig('test')
+print("Running K means elbow method for Dataset 1")
+ds1_y = calc_kmeans(dataset1, k_range)
+save_plot("K vs Loss on Dataset 1", "Number of Clusters (K)", "Average Loss", k_range, ds1_y)
