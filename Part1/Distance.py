6e418dc2cf1d4d63145f02a671ea2d9584f45ad7
berkulutas <berkulutas2001@gmail.com>
2024-11-22 04:40:17 +0300

    distance update

--- Part1/Distance.py
+++ Part1/Distance.py
@@ -7,8 +7,9 @@ class Distance:
         return 1 - np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
     @staticmethod
     def calculateMinkowskiDistance(x, y, p=2):
-        return np.sum((x-y)**p)**(1/p)
+        return np.sum(abs(x-y)**p)**(1/p) # TODO check overflow when p big
     @staticmethod
     def calculateMahalanobisDistance(x,y, S_minus_1):
-        return np.sqrt(np.dot(np.dot((x-y).T, S_minus_1), (x-y)))
+        delta = (x-y)
+        return np.sqrt(np.dot(np.dot(delta.T, S_minus_1), delta))
 
