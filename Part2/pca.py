cc6ed7de05065d382cc167d8fff06fc586c3753e
berkulutas <berkulutas2001@gmail.com>
2024-11-26 00:19:28 +0300

    inital pca implementation

--- Part2/pca.py
+++ Part2/pca.py
@@ -9,6 +9,7 @@ class PCA:
         self.projection_dim = projection_dim
         # keeps the projection matrix information
         self.projection_matrix = None
+        self.mean = None
 
     def fit(self, x: np.ndarray) -> None:
         """
@@ -18,6 +19,23 @@ class PCA:
 
         this function should assign the resulting projection matrix to self.projection_matrix
         """
+        # calculate mean of each column
+        self.mean = np.mean(x, axis=0)
+        # center columns by subtracting column means
+        centered_data = x - self.mean
+        # calculate covariance matrix of centered matrix
+        covariance_matrix = np.cov(centered_data.T)
+        # eigenvector and eigenvalues
+        eig_values, eig_vectors = np.linalg.eig(covariance_matrix)
+
+        # sort eigenvectors based on eigenvalues
+        idx = eig_values.argsort()[::-1]
+        eig_values = eig_values[idx]
+        eig_vectors = eig_vectors[:, idx]
+
+        # select the first k eigenvectors
+        self.projection_matrix = eig_vectors[:, :self.projection_dim]
+
 
     def transform(self, x: np.ndarray) -> np.ndarray:
         """
@@ -27,3 +45,30 @@ class PCA:
         :return: transformed (projected) data instances (projected data matrix)
         this function should utilize self.projection_matrix for the operations
         """
+        # center columns by subtracting column means
+        centered_data = x - self.mean
+        # project data
+        return centered_data.dot(self.projection_matrix)
+    
+
+# test the implementation
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    from sklearn.datasets import load_iris
+
+    data = load_iris()
+
+    X = data.data
+    y = data.target
+
+    pca = PCA(2)
+
+    pca.fit(X)
+    X_pca = pca.transform(X)
+
+    print(f"shape of X: {X.shape}")
+    print(f"shape of X_pca: {X_pca.shape}")
+
+    plt.scatter(X_pca[:, 0], X_pca[:, 1], c=y)
+    plt.show()
