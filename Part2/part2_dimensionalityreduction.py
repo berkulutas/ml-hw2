fa73a68216f31f2b6f52916c70a037b3c2d4a4cb
berkulutas <berkulutas2001@gmail.com>
2024-11-30 14:32:51 +0300

    part2 adjustment for report

--- Part2/part2_dimensionalityreduction.py
+++ Part2/part2_dimensionalityreduction.py
@@ -11,17 +11,13 @@ from umap import UMAP
 dataset1 = pickle.load(open("../datasets/part2_dataset_1.data", "rb"))
 dataset2 = pickle.load(open("../datasets/part2_dataset_2.data", "rb"))
 
-# # TODO remove old dataset
-# dataset1 = pickle.load(open("../data2023/part2_dataset_1.data", "rb"))
-# dataset2 = pickle.load(open("../data2023/part2_dataset_2.data", "rb"))
-
 def visualize_pca(dataset: np.ndarray, title: str) -> None:
     pca = PCA(2)
     pca.fit(dataset)
     transformed_data = pca.transform(dataset)
     plt.scatter(transformed_data[:, 0], transformed_data[:, 1])
     plt.title(title)
-    plt.savefig(f"plots/{title}.png")
+    plt.savefig(f"dr_plots/{title}.png")
     plt.close()
 
 def visualize_autoencoder(dataset: np.ndarray, title: str) -> None:
@@ -30,7 +26,7 @@ def visualize_autoencoder(dataset: np.ndarray, title: str) -> None:
     projected_data = autoencoder.fit_transform(dataset_tensor).detach().numpy()
     plt.scatter(projected_data[:, 0], projected_data[:, 1])
     plt.title(title)
-    plt.savefig(f"plots/{title}.png")
+    plt.savefig(f"dr_plots/{title}.png")
     plt.close()
 
 def visualize_tsne(dataset: np.ndarray, title: str, n_components=2, perplexity=30) -> None:
@@ -38,50 +34,42 @@ def visualize_tsne(dataset: np.ndarray, title: str, n_components=2, perplexity=3
     transformed_data = tsne.fit_transform(dataset)
     plt.scatter(transformed_data[:, 0], transformed_data[:, 1])
     plt.title(title)
-    plt.savefig(f"plots/{title}.png")
+    plt.savefig(f"dr_plots/{title}.png")
     plt.close()
 
 def visualize_umap(dataset: np.ndarray, title: str, n_components=2, min_dist=0.1, n_neighbours=15, metric='euclidean') -> None:
-    umap = UMAP(n_components=2, )
+    umap = UMAP(n_components=n_components, min_dist=min_dist, n_neighbors=n_neighbours, metric=metric)
     transformed_data = umap.fit_transform(dataset)
     plt.scatter(transformed_data[:, 0], transformed_data[:, 1])
     plt.title(title)
-    plt.savefig(f"plots/{title}.png")
+    plt.savefig(f"dr_plots/{title}.png")
     plt.close()
 
 
-# print("visualizing PCA...")
-# visualize_pca(dataset1, "PCA on Dataset 1")
-# visualize_pca(dataset2, "PCA on Dataset 2")
+print("visualizing PCA...")
+visualize_pca(dataset1, "PCA on Dataset 1")
+visualize_pca(dataset2, "PCA on Dataset 2")
 
-# print("visualizing Autoencoder...")
-# visualize_autoencoder(dataset1, "Autoencoder on Dataset 1")
-# visualize_autoencoder(dataset2, "Autoencoder on Dataset 2")
+print("visualizing Autoencoder...")
+visualize_autoencoder(dataset1, "Autoencoder on Dataset 1")
+visualize_autoencoder(dataset2, "Autoencoder on Dataset 2")
 
-# print("visualizing t-SNE...")
-# visualize_tsne(dataset1, "t-SNE on Dataset 1 # of components 2 (perplexity=30)", perplexity=30)
-# visualize_tsne(dataset2, "t-SNE on Dataset 2 # of components 2 (perplexity=30)", perplexity=30)
+print("visualizing t-SNE...")
+visualize_tsne(dataset1, "t-SNE on Dataset 1 (Components 2, Perplexity=30)", perplexity=30)
+visualize_tsne(dataset2, "t-SNE on Dataset 2 (Components 2, Perplexity=30)", perplexity=30)
 
-# print("visualizing t-SNE...")
-# visualize_tsne(dataset1, "t-SNE on Dataset 1 # of components 2 (perplexity=50)", perplexity=50)
-# visualize_tsne(dataset2, "t-SNE on Dataset 2 # of components 2 (perplexity=50)", perplexity=50)
+print("visualizing t-SNE...")
+visualize_tsne(dataset1, "t-SNE on Dataset 1 (Components 2, Perplexity=50)", perplexity=50)
+visualize_tsne(dataset2, "t-SNE on Dataset 2 (Components 2, Perplexity=50)", perplexity=50)
 
 print("visualizing UMAP...")
-visualize_umap(dataset1, "UMAP on Dataset 1")
-visualize_umap(dataset2, "UMAP on Dataset 2")
 
-visualize_umap(dataset1, "UMAP on Dataset 1 cosine", metric='cosine')
-visualize_umap(dataset2, "UMAP on Dataset 2 cosine", metric='cosine')
+visualize_umap(dataset1, "UMAP on Dataset 1 (Distance cosine, Neighbors 15)", n_neighbours=15, metric='cosine')
+visualize_umap(dataset2, "UMAP on Dataset 2 (Distance cosine, Neighbors 15)", n_neighbours=15, metric='cosine')
 
-visualize_umap(dataset1, "UMAP on Dataset 1 euclidean", metric='euclidean')
-visualize_umap(dataset2, "UMAP on Dataset 2 euclidean", metric='euclidean')
-
-visualize_umap(dataset1, "UMAP on Dataset 1 cosine # of neighbours 30", n_neighbours=30, metric='cosine')
-visualize_umap(dataset2, "UMAP on Dataset 2 cosine # of neighbours 30", n_neighbours=30, metric='cosine')
-
-visualize_umap(dataset1, "UMAP on Dataset 1 euclidean # of neighbours 30", n_neighbours=30, metric='euclidean')
-visualize_umap(dataset2, "UMAP on Dataset 2 euclidean # of neighbours 30", n_neighbours=30, metric='euclidean')
+visualize_umap(dataset1, "UMAP on Dataset 1 (Distance euclidean, Neighbors 15)", n_neighbours=15, metric='euclidean')
+visualize_umap(dataset2, "UMAP on Dataset 2 (Distance euclidean, Neighbors 15)", n_neighbours=15, metric='euclidean')
 
 
 
-# print("done!! :)")
+print("done!! :)")
